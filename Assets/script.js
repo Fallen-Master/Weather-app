61dadee20f0b2a071a525ae701dd6bd4c107a7dd
Rafael Gomez <rgomez2014@gmail.com>
2023-07-23 13:26:35 -0500

    updated HTML to cards/ search can save city name

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,32 @@
+var key = '2f23027a9f86d417da7e31e6ab9ea170'
+
+document.getElementById('search-city')
+
+.addEventListener('click', function(event) {
+    event.preventDefault();
+    var cityName = document.querySelector('input').value;
+    document.querySelector('input').value =''
+    console.log(cityName)
+    fetch ()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var fetchButton = document.getElementById('fetch-button');
+
 
 function getApi(){
     let requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2f23027a9f86d417da7e31e6ab9ea170'
