53b79b6320a3563bac9a23f3470b05c3bf492abc
Rafael Gomez <rgomez2014@gmail.com>
2023-07-23 14:41:58 -0500

    lat and lon/ history

--- Assets/script.js
+++ Assets/script.js
@@ -7,7 +7,25 @@ document.getElementById('search-city')
     var cityName = document.querySelector('input').value;
     document.querySelector('input').value =''
     console.log(cityName)
-    fetch ()
+    fetch (`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${key}`)
+    .then(response => response.json())
+    .then(data => {
+        var lat = data[0].lat;
+        var lon = data[0].lon;
+        console.log(`Latitude: ${lat}, Longitude: ${lon}`)
+
+        var ul = document.querySelector("ul");
+        var li = document.createElement("li");
+        li.textContent = cityName;
+        ul.appendChild(li); 
+
+       
+        var cityHistory = JSON.parse(localStorage.getItem("cityHistory")) || [];
+        cityHistory.push(cityName);
+        localStorage.setItem("cityHistory", JSON.stringify(cityHistory));
+      
+
+    })
 })
 
 
@@ -25,19 +43,19 @@ document.getElementById('search-city')
 
 
 
-var fetchButton = document.getElementById('fetch-button');
+// var fetchButton = document.getElementById('fetch-button');
 
 
-function getApi(){
-    let requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2f23027a9f86d417da7e31e6ab9ea170'
+// function getApi(){
+//     let requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2f23027a9f86d417da7e31e6ab9ea170'
 
-    fetch(requestUrl)
-    .then(function (response) {
-        return response.json()
-        console.log(response)
-    })
-    .then (function (data){
-        console.log(data)
-    })
-}
-fetchButton.addEventListener('click', getApi);
+//     fetch(requestUrl)
+//     .then(function (response) {
+//         return response.json()
+//         console.log(response)
+//     })
+//     .then (function (data){
+//         console.log(data)
+//     })
+// }
+// fetchButton.addEventListener('click', getApi);
